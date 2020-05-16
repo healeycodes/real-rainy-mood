ecfe5e18c34c53b0b8eec7e6a883436922cc3de1
Andrew <healeycodes@gmail.com>
2020-05-16 20:50:04 +0100

    Fix pretty location string

--- functions/weather.js
+++ functions/weather.js
@@ -3,7 +3,10 @@ const fetch = require("node-fetch");
 exports.handler = async function(event, context, callback) {
   const ip = event.headers["client-ip"];
   const location = await fetch(`http://ip-api.com/json/${ip}`).then((res) => res.json());
-  const prettyLocation = `${location.zip}\n${location.city}\n${location.regionName}\n${location.country}`;
+  const prettyLocation = `${location.zip}
+${location.city}
+${location.regionName}
+${location.country}`;
 
   const current_forecast = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <h1>Welcome!</h1>
-    <NLink to="/about">
-      About Page
-    </NLink>
+    <NLink to="/about">About Page</NLink>
+
+    <client-only placeholder="Loading...">
+      <!-- this component will only be rendered on client-side -->
+      Here we'll make a call to the Netlify function.
+    </client-only>
   </div>
 </template>
 
 <script>
 export default {
   head: {
-    title: 'Home page'
+    title: "Home page"
   }
-}
+
+};
 </script>
