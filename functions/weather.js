bfd33a28e6029962ca6304b840b27fffcfe0e8fd
Andrew <healeycodes@gmail.com>
2020-05-16 16:44:39 +0100

    Fix non-absolute URL

--- functions/weather.js
+++ functions/weather.js
@@ -4,11 +4,9 @@ exports.handler = async function(event, context, callback) {
   const ip = event.headers["client-ip"];
   const location = await fetch(`http://ip-api.com/json/${ip}`).then((res) => res.json());
   const prettyLocation = `${location.zip}\n${location.city}\n${location.regionName}\n${location.Canada}`;
-  const lat = location.lat;
-  const lon = location.lon;
 
   const current_forecast = await fetch(
-    `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid${process.env.OPEN_WEATHER_MAP_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid${process.env.OPEN_WEATHER_MAP_API_KEY}`
   ).then((res) => res.json());
   const weatherId = current_forecast.weather[0].id;
 
@@ -20,6 +18,6 @@ exports.handler = async function(event, context, callback) {
 
   callback(null, {
     statusCode: 200,
-    body: raining,
+    body: { raining, location, prettyLocation, current_forecast },
   });
 };
