0a0190622bc7951aec90b14819be3f3a061d9989
Andrew <healeycodes@gmail.com>
2020-05-17 14:24:41 +0100

    Add more text to about page

--- pages/about.vue
+++ pages/about.vue
@@ -6,9 +6,10 @@
       <a href="https://healeycodes.com/">Andrew</a> here.
     </h1>
     <p>I wanted to learn more about Nuxt.js so I built this thing you're reading.</p>
+    <p>It uses Netlify serverless functions, IP-API.com, and OpenWeatherMap.</p>
     <p>
       Check out the
-      <a href="https://github.com/healeycodes/real-rainy-mood">source code</a>.
+      <a href="https://github.com/healeycodes/real-rainy-mood">source code</a>!
     </p>
   </div>
 </template>
